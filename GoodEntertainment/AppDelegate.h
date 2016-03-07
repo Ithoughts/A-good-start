a81ef87bba5da40a6144081bc2cdeb6ad05d6c02
onvcat <yxw6666@gmail.com>
2016-03-07 17:17:36 +0800

    Initial Commit

---
+++ GoodEntertainment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoodEntertainment
+//
+//  Created by Xiaowu Yin on 3/7/16.
+//  Copyright © 2016 Xiaowu Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoodEntertainment/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoodEntertainment
+//
+//  Created by Xiaowu Yin on 3/7/16.
+//  Copyright © 2016 Xiaowu Yin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
